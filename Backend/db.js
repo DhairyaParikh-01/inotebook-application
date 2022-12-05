1582c05686b1d6084385b80fea9690991fcfeef9
Dhairya <dhairyaparikh1002@gmail.com>
2022-12-05 09:13:36 +0530

    1.) Successfully tested mongo db connection. 2.) Succesfully tested end point by creating a basic express server.

---
+++ Backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongooseURI = "mongodb://localhost:27017";
+
+const connect_to_mongo = () => {
+    mongoose.connect(mongooseURI, () =>{
+        console.log("Connected to Mongo DB successflly!");
+    })
+}
+
+module.exports = connect_to_mongo;
